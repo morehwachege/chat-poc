55a9e100a237a3f13f6f96994184f2f96feda52e
Antony Gakuru <antony123muriithi@gmail.com>
2026-02-13 19:49:28 +0300

    add connect

--- lib/cable.ts
+++ lib/cable.ts
@@ -1,6 +1,8 @@
 import { createConsumer } from "@rails/actioncable"
 
+const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMDE5YzM5MTItMjEzZC03ZDZlLWI3NDEtMTIwYWJlM2EyYzRmIiwic2Vzc2lvbl9pZCI6IjAxOWM1N2RhLTA3OTktNzFhMS04ZmM4LWY4MjU0MTRiYmI1ZCIsImV4cCI6MTc3MTAwMTMzMn0.eOOLtBEDv4pb_A-2Smz6Et16GKluB-PCn2w4Yiz5nfg"
+
 const CABLE_URL = 
-    process.env.NEXT_PUBLIC_CABLE_URL || "ws:localhost:3000/cable?token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMDE5YzM5MTItMjEzZC03ZDZlLWI3NDEtMTIwYWJlM2EyYzRmIiwic2Vzc2lvbl9pZCI6IjAxOWM1N2NmLTgwNzktNzFmNy1iOWY2LTgwOGYxYWY4OGI4NiIsImV4cCI6MTc3MTAwMDY0Mn0.Gxi-5RbuT4V_eoPPbGnPAnaLUHli302omi-TmLxkQag"
+    process.env.NEXT_PUBLIC_CABLE_URL || `ws:localhost:3000/cable?token=${token}`
 
 export const cable = createConsumer(CABLE_URL)
