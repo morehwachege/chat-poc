9f4c265ec4a70836a9dd2ef799b25ae57e7531e5
Antony Gakuru <antony123muriithi@gmail.com>
2026-02-14 15:46:48 +0300

    add formatting

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,29 +1,31 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import GetUser from "./getUser";
 
 function Home() {
-    const router = useRouter();
-    const [user, setUser] = useState<string | null>(null);
-    const [checkingAuth, setCheckingAuth] = useState(true);
-     useEffect(() => {
-       const storedUser = localStorage.getItem("user");
+  const router = useRouter();
+  const [user, setUser] = useState<string | null>(null);
+  const [checkingAuth, setCheckingAuth] = useState(true);
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
 
-       if (!storedUser) {
-         router.replace("/");
-       } else {
-         setUser(storedUser);
-       }
+    if (!storedUser) {
+      router.replace("/");
+    } else {
+      setUser(storedUser);
+    }
 
-       setCheckingAuth(false);
-     }, [router]);
+    setCheckingAuth(false);
+  }, [router]);
 
-    if(checkingAuth) return null
-  return <div>
-    <GetUser />
-    <p>This is something new</p>
-  </div>;
+  if (checkingAuth) return null;
+  return (
+    <div>
+      <GetUser />
+      <p>This is something new</p>
+    </div>
+  );
 }
 
 export default Home;
