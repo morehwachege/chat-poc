5a7536eb4f089289b2de9c21d2b333f10e58fc77
Antony Gakuru <antony123muriithi@gmail.com>
2026-02-14 12:30:28 +0300

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Login from "./components/Login";
 
-export default function Home() {
+export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="w-200 h-120 rounded-md border border-gray-600 bg-[#1F2535] pt-10 flex justify-center flex-col">
