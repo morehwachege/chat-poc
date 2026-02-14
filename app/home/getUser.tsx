12e84101d47b58724cd3f0320b4f0bc2d3b4ba56
Antony Gakuru <antony123muriithi@gmail.com>
2026-02-14 15:01:24 +0300

    fx item

--- app/home/getUser.tsx
+++ app/home/getUser.tsx
@@ -1,8 +1,36 @@
 "use client";
+import { BASE_URL } from "@/lib/baseUrl";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 function GetUser() {
+    const accessToken = localStorage.getItem("tk");
+    async function handleGetUser(){
+        try {
+          const response = await fetch(`${BASE_URL}/auth/me`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+
+          if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || "Login failed");
+          }
+
+          const data = await response.json();
+          console.log("User: ", data)
+          localStorage.setItem("user", data);
+
+          return data;
+        } catch (error: any) {
+          console.error("Login error:", error.message);
+          throw error;
+        }
+    }
   
   return <div></div>;
 }
